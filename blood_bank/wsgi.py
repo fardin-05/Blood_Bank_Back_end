d146f7d7cfb4608bce068244a227fd6584e79c95
Fardin Khan <azimahmed9722@gmail.com>
2025-12-09 00:36:24 +0600

    fix procfile for render,

--- blood_bank/wsgi.py
+++ blood_bank/wsgi.py
@@ -1,9 +1,7 @@
-
-
 import os
 
 from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'blood_bank.settings')
 
-app = get_wsgi_application()
+application = get_wsgi_application()
