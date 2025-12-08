ad91778116af309545f87ad8c49488c7d77e721d
Fardin Khan <azimahmed9722@gmail.com>
2025-12-08 23:12:48 +0600

    commited

--- api.py
+++ api.py
@@ -1,11 +1,12 @@
 import os
 import sys
-from django.core.wsgi import get_wsgi_application
+
+sys.path.insert(0, os.path.dirname(__file__)) 
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'blood_bank.settings')
 
-sys.path.insert(0, os.path.dirname(__file__))
+from django.core.wsgi import get_wsgi_application
 
 application = get_wsgi_application()
 
-handler = application
+handler = application 
