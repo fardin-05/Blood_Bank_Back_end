c25940393b38903d241316ca3c57645050565145
Fardin Khan <azimahmed9722@gmail.com>
2025-12-09 00:22:49 +0600

    commited

--- blood_bank/wsgi.py
+++ blood_bank/wsgi.py
@@ -6,4 +6,4 @@ from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'blood_bank.settings')
 
-application = get_wsgi_application()
+app = get_wsgi_application()
