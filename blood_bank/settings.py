89823ea287cd59dbbc0ba2a393c3db1294fc3818
Fardin Khan <azimahmed9722@gmail.com>
2026-05-05 00:07:06 +0600

    Update settings.py

--- blood_bank/settings.py
+++ blood_bank/settings.py
@@ -4,7 +4,7 @@ import dj_database_url
 from datetime import timedelta
 import os
 
-# ================= BASE =================
+
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 SECRET_KEY = config("SECRET_KEY")
